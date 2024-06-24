a46707c7e8330e81c391ffe9f10a75ea75f9c58c
Daniel Persson <mailto.woden@gmail.com>
2024-06-24 06:21:46 +0200

    Bug fixing.

--- bin/updateActive.php
+++ bin/updateActive.php
@@ -1,6 +1,18 @@
 <?php
 require_once(__DIR__ . '/../include/dbconnect.php');
 
+$stmt = $mysqli->prepare(
+    'UPDATE block SET active = false, changed = true WHERE (startTime > now() OR endTime < now()) AND active = true'
+);
+$stmt->execute();
+
+$stmt = $mysqli->prepare(
+    'UPDATE block SET active = true, changed = true WHERE ' . 
+    ' (startTime is null OR startTime <= now()) AND ' .
+    ' (endTime is null OR endTime >= now()) AND active = false'
+);
+$stmt->execute();
+
 $stmt = $mysqli->prepare('SELECT * FROM block WHERE override_categories = true AND changed = true');
 $stmt->execute();
 $result = $stmt->get_result();
@@ -30,14 +42,5 @@ if ($result->num_rows > 0) {
     }
 }
 
-$stmt = $mysqli->prepare(
-    'UPDATE block SET active = false, changed = false WHERE (startTime > now() OR endTime < now()) AND changed = true'
-);
-$stmt->execute();
-
-$stmt = $mysqli->prepare(
-    'UPDATE block SET active = true, changed = false WHERE ' . 
-    ' (startTime is null OR startTime <= now()) AND ' .
-    ' (endTime is null OR endTime >= now()) AND changed = true'
-);
+$stmt = $mysqli->prepare('UPDATE block SET changed = false');
 $stmt->execute();
